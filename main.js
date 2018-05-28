c743ba11adc40428415b0908c3951c36bc0b98cd
Anders Verdier <Andersverdier@hotmail.com>
2018-05-28 12:00:21 +0200

    øvelse 2

--- main.js
+++ main.js
@@ -1,52 +1,20 @@
 "use strict";
-// String fra DOM
-let team = document.querySelector("#teamname").textContent;
-// Bare en string
-let teamString = `TeamDubbiHansi`;
 
-//convert to array
-let arr = Array.from ( team );
+document.addEventListener("DOMContentLoaded", addStudents);
 
-/*
-//for - Loop iterates through the array
-let i;
-for(i = 0; i < arr.length; i++) {
-    writeLetter(arr[6]);
-};
+async function addStudents(){
 
-//forEach that iterates through each
-arr.forEach(writeLetter);
+    let template = document.querySelector("#studentsinfo");
+    let students = document.querySelector("#students");
 
+    let jsonData = await fetch("data.json");
+    let names = await jsonData.json();
+    console.log(names);
 
-//function for writing a single letter to console
-function writeLetter(letters) {
-console.log(letters)
-};
+    names.forEach(function(parm){
+        let splitNames = parm.split(" ");
+        console.log(names);
+    })
 
-
-//loop through the array -- forEach w. anonymous function
- arr.forEach(function(array){
-    console.log(array)
-}); 
-*/
-
-
-//arrow function
-arr.forEach(array => {
-    console.log(array);
-}) 
-
-
-//make string uppercase
-function upper(upperText) {
-    return upperText.toUpperCase();
-};
-upper(teamString);
-
-
-let map = arr.map(upper);
-console.log(map)
-
-let newString = map.join('');
-console.log(newString)
+}
 

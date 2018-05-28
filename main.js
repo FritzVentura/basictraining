f7bdc2ac45e5b5c082f17a259c59cb6d149c7e4b
Anders Verdier <Andersverdier@hotmail.com>
2018-05-28 09:30:09 +0200

    begginnings

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-let team = [`TeamDubbiHansi`]
+
+// String fra DOM
+let team = document.querySelector("#teamname").textContent;
+// Bare en string
+let teamString = `TeamDubbiHansi`;
+
+//convert to array
+let arr = Array.from ( team );
+
+//loop through the array
+
+arr.forEach(function(){
+    console.log()
+})

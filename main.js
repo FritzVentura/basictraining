905732facda9ead21a218dfcdca330e695e535a7
Anders Verdier <Andersverdier@hotmail.com>
2018-05-28 11:34:02 +0200

    øvelse1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+"use strict";
 // String fra DOM
 let team = document.querySelector("#teamname").textContent;
 // Bare en string
@@ -7,8 +7,46 @@ let teamString = `TeamDubbiHansi`;
 //convert to array
 let arr = Array.from ( team );
 
-//loop through the array
+/*
+//for - Loop iterates through the array
+let i;
+for(i = 0; i < arr.length; i++) {
+    writeLetter(arr[6]);
+};
 
-arr.forEach(function(){
-    console.log()
-})
+//forEach that iterates through each
+arr.forEach(writeLetter);
+
+
+//function for writing a single letter to console
+function writeLetter(letters) {
+console.log(letters)
+};
+
+
+//loop through the array -- forEach w. anonymous function
+ arr.forEach(function(array){
+    console.log(array)
+}); 
+*/
+
+
+//arrow function
+arr.forEach(array => {
+    console.log(array);
+}) 
+
+
+//make string uppercase
+function upper(upperText) {
+    return upperText.toUpperCase();
+};
+upper(teamString);
+
+
+let map = arr.map(upper);
+console.log(map)
+
+let newString = map.join('');
+console.log(newString)
+
